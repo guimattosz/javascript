f6d310799b415c13036ea8b72687e411c228db33
Guilherme Mattos <guilherme_mattos96@hotmail.com>
2024-09-13 21:32:52 -0300

    Estudo das Variaveis

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de Constante e Variáveis
+ * @author Guilherme Mattos
+ */
+
+console.clear()
+
+// Variáveis
+let nome 
+let idade 
+let vip 
+
+nome = "Guilherme Mattos" //String
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 28 //Number
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true //Boolean
+console.log(typeof(vip))
+console.log(vip)
